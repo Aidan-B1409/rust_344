fc967bff12aa8bc5156087ee9b6e5345082e32cd
Aidan-B1409 <aidanb1049@gmail.com>
2021-08-04 20:38:13 -0700

    refactored fahr to cel

--- inclass_2_fahr_to_cel/src/main.rs
+++ inclass_2_fahr_to_cel/src/main.rs
@@ -8,11 +8,21 @@ fn main() {
         .read_line(&mut input)
         .expect("Failed to read user input");
 
-    let fahr = input
+    let fahr = parse_string(&input);
+
+    let celcius = fahr_to_cel(fahr);
+    println!("Celcius = {:0.2}", celcius);
+}
+
+fn fahr_to_cel(fahr: f32) -> f32 {
+    (fahr - 32.0) * 5.0 / 9.0
+}
+
+fn parse_string(input: &str) -> f32 {
+    let num = input
         .trim()
         .parse::<f32>()
         .expect("User input is not a number");
-
-    let celcius = (fahr - 32.0) * 5.0 / 9.0;
-    println!("Celcius = {:0.2}", celcius)
+    return num
 }
+
